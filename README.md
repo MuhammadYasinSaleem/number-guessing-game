2ff07a09391c7fc5d069fb928cf6005e730b84ca
MuhammadYasinSaleem <m.yaseensalim@gmail.com>
2023-10-22 23:28:01 +0500

    second

---
+++ README.md
@@ -0,0 +1,5 @@
+#Number Guessing Game
+This project is build using typescript, node.js, inquirer, chalk, chalk-animation
+Asks the user to guess a number between 1 and 50. The program then tells them if their guess is correct or wrong.
+
+#installation
---
+++ game.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation"
+
+const sleep = () => {
+    return new Promise((s) => {
+        setTimeout(s, 2000);
+    })
+}
+
+async function welcome() {
+    let a = chalkAnimation.rainbow("Number Guessing Game");
+    await sleep();
+    a.stop();
+}
+async function call() {
+    await welcome();
+    let ans = await inquirer.prompt({
+        type: "input",
+        name: "no",
+        message: chalk.greenBright("Enter your number from 1 and 50"),
+        validate: (answer) => {
+            if (answer > 50 || answer < 1 || isNaN(answer)) {
+                return "Please enter number from 1 to 50";
+            }
+            return true;
+
+        }
+    })
+    const num = Math.floor(Math.random() * 10);
+    if (ans.no == num) {
+        console.log(chalk.bgBlueBright(num + " is the correct number"));
+        console.log(`${chalk.blueBright('Congratulations You guessed the correct number')} `);
+    }
+    else {
+        console.log(chalk.bgRedBright(num + " is the correct number"));
+        console.log(`${chalk.redBright('Sorry! Better Luck next time')}`);
+    }
+}
+
+call();
